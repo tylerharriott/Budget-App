801111d168cf619446f3a53e7be355a491a17613
Tyler Harriott <tharriott02@gmail.com>
2019-05-21 14:21:06 -0400

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,51 @@
+ var budgetController = (function (){
+
+
+     var x = 23;
+
+
+
+     var add = function (a){
+
+       return x + a ;
+
+     }
+
+
+     return {
+         publicTest: function(b){
+             return add(b);
+         }
+     }
+
+
+ }) ();
+
+
+
+
+
+var UIController = (function (){
+
+    //Some code for later
+
+}) ();
+
+
+
+
+
+var controller = (function(budgetCtrl,UICtrl){
+
+
+
+
+var z =budgetCtrl.publicTest(5);
+return {
+    anotherPublicMethod: function (){
+        console.log(z);
+    }
+}
+
+
+}) (budgetController,UIController  );
